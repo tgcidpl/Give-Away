d5641d7657690bd2e0bd608ca5cee8111d0c76e9
tgcidpl <tgcidpl@gmail.com>
2022-11-16 20:03:19 +0100

    NavHeader done

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import './Cta.scss';
-import { DecoratedHeader } from "../DecoratedHeader/DecoratedHeader.jsx"
-import { LargeBtn } from "../LargeBtn/LargeBtn.jsx"
+import {DecoratedHeader} from "../DecoratedHeader/DecoratedHeader.jsx"
+import {LargeBtn} from "../LargeBtn/LargeBtn.jsx"
 
-export function Cta () {
+export function Cta() {
     return (
         <div className='Cta'>
             <DecoratedHeader text1={`Zacznij pomagać!`}
                              text2={`Oddaj niechciane rzeczy w zaufane ręce`}
-                              />
-            <LargeBtn text={`ODDAJ RZECZY`} link={`/login`} />
-            <LargeBtn text={`ZORGANIZUJ ZBIÓRKĘ`} link={`/login`} />
+            />
+            <div className='Cta-btns'>
+                <LargeBtn text={`ODDAJ RZECZY`} link={`/login`}/>
+                <LargeBtn text={`ZORGANIZUJ ZBIÓRKĘ`} link={`/login`}/>
+            </div>
         </div>
     )
 }
--- src/components/LargeBtn/LargeBtn.jsx
+++ src/components/LargeBtn/LargeBtn.jsx
@@ -3,9 +3,5 @@ import './LargeBtn.scss';
 import { Link } from "react-router-dom";
 
 export function LargeBtn ({link, text}) {
-    return (
-        <div className="LargeBtn">
-            <Link className='LargeBtn-link' to={link}>{text}</Link>
-        </div>
-    )
+    return <Link className='LargeBtn' to={link}>{text}</Link>
 }
