55dee43f3cf67a1b4e88a46900644ce142333b42
tgcidpl <tgcidpl@gmail.com>
2022-11-16 20:27:18 +0100

    ThreeColumns WIP

---
+++ src/components/ThreeColumns/ThreeColumns.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './ThreeColumns.scss'
+
+export function ThreeColumns() {
+    return (
+        <div className='ThreeColumns'>
+        </div>
+    )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import {NavHeader} from "../components/NavHeader/NavHeader.jsx"
+import { NavHeader } from "../components/NavHeader/NavHeader.jsx"
+import { ThreeColumns } from "../components/ThreeColumns/ThreeColumns.jsx"
 
 export function Home() {
     return (
         <>
-            <NavHeader/>
+            <NavHeader />
+            <ThreeColumns />
         </>
     )
 }
