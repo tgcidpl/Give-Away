16ad0749c6e006fbdd2c09a360d5a165c3c1cd8f
tgcidpl <tgcidpl@gmail.com>
2022-11-17 15:19:46 +0100

    WhatsItAbout done

--- src/components/WhatsItAbout/WhatsItAbout.jsx
+++ src/components/WhatsItAbout/WhatsItAbout.jsx
@@ -9,19 +9,19 @@ export function WhatsItAbout () {
             <DecoratedHeader text2={'Just 4 easy steps'}/>
             <div className='WhatsItAbout-info'>
                 <div className='WhatsItAbout-info-column'>
-                    <h3 className='WhatsItAbout-info-column__title'>Pick things</h3>
+                    <h3 className='WhatsItAbout-info-column__title title1'>Pick things</h3>
                     <p className='WhatsItAbout-info-column__text'>clothes, toys, tools and others</p>
                 </div>
                 <div className='WhatsItAbout-info-column'>
-                    <h3 className='WhatsItAbout-info-column__title'>Pack them</h3>
+                    <h3 className='WhatsItAbout-info-column__title title2'>Pack them</h3>
                     <p className='WhatsItAbout-info-column__text'>use bin bags</p>
                 </div>
                 <div className='WhatsItAbout-info-column'>
-                    <h3 className='WhatsItAbout-info-column__title'>Decide who to help</h3>
+                    <h3 className='WhatsItAbout-info-column__title title3'>Decide who to help</h3>
                     <p className='WhatsItAbout-info-column__text'>choose a trusted place</p>
                 </div>
                 <div className='WhatsItAbout-info-column'>
-                    <h3 className='WhatsItAbout-info-column__title'>Book a courier</h3>
+                    <h3 className='WhatsItAbout-info-column__title title4'>Book a courier</h3>
                     <p className='WhatsItAbout-info-column__text'>courier will arrive at a suitable time</p>
                 </div>
             </div>
