850da64619b14f243298079f72435e52b249910f
tgcidpl <tgcidpl@gmail.com>
2022-11-18 12:23:58 +0100

    AboutUs done

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./AboutUs.scss";
+import { DecoratedHeader } from "../DecoratedHeader/DecoratedHeader";
+
+export function AboutUs() {
+  return (
+    <div className="AboutUs">
+      <div className="AboutUs-description">
+				<DecoratedHeader text2='About us' />
+				<p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+			</div>
+      <div className="AboutUs-image"></div>
+    </div>
+  );
+}
