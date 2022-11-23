38eaea2ec336c0cf884dfad2a3fd6010190a479f
tgcidpl <tgcidpl@gmail.com>
2022-11-23 15:28:29 +0100

    ContactForm validation wip

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,9 +3,22 @@ import { DecoratedHeader } from "../DecoratedHeader/DecoratedHeader";
 import "./Contact.scss";
 
 export const Contact = () => {
-  const [name, setName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [message, setMessage] = useState(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    name.includes(" ") || name.length === 0
+      ? setNameError(true)
+      : setNameError(false);
+    !email.includes("@") ? setEmailError(true) : setEmailError(false);
+    message.length < 120 ? setMessageError(true) : setError(false);
+    console.log(name, email, message);
+  };
 
   return (
     <div className="Contact">
@@ -25,16 +38,18 @@ export const Contact = () => {
                     value={name}
                     placeholder="Krzysztof"
                   ></input>
+                  {nameError ? <label>Incorrect name</label> : ""}
                 </div>
                 <div className="labelInput">
                   <label className="label">Enter your email</label>
                   <input
                     onChange={(e) => setEmail(e.target.value)}
-                    type="text"
+                    type="email"
                     className="Contact-form-data-info__field"
                     value={email}
                     placeholder="abc@xyz.pl"
                   ></input>
+                  {emailError ? <label>Incorrect email</label> : ""}
                 </div>
               </div>
               <div className="labelInput">
@@ -46,8 +61,14 @@ export const Contact = () => {
                   rows={4}
                   placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
                 ></textarea>
+                {messageError ? (
+                  <label>Message should be longer than 120 signs</label>
+                ) : (
+                  ""
+                )}
               </div>
               <input
+                onClick={(e) => handleSubmit(e)}
                 type="submit"
                 className="Contact-form-data__btn"
                 value="Send"
