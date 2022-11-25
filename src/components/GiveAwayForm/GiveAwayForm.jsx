1baa97637020036c9a8f728744851840bde0296a
tgcidpl <tgcidpl@gmail.com>
2022-11-25 18:22:35 +0100

    GiveAwayForm WIP

---
+++ src/components/GiveAwayForm/GiveAwayForm.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./GiveAwayForm.scss";
+
+export const GiveAwayForm = () => {
+  return <div>GiveAwayForm</div>;
+};
--- src/pages/give-away.jsx
+++ src/pages/give-away.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Contact } from "../components/Contact/Contact.jsx";
+import { Footer } from "../components/Footer/Footer.jsx";
+import { GiveAwayForm } from "../components/GiveAwayForm/GiveAwayForm.jsx";
 import { GiveAwayNavHeader } from "../components/GiveAwayNavHeader/GiveAwayNavHeader.jsx";
 
 export function GiveAway() {
   return (
     <>
       <GiveAwayNavHeader />
+      <GiveAwayForm />
       <Contact />
+      <Footer />
     </>
   );
 }
